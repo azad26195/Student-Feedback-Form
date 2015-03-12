2d991a686cfa10182b5b924b86bf42a2d58351f7
admin <admin@azad-2.local>
2015-03-12 16:22:45 +0530

    Initial Commit

---
+++ Student Feedback FormTests/Student_Feedback_FormTests.swift
@@ -0,0 +1,36 @@
+//
+//  Student_Feedback_FormTests.swift
+//  Student Feedback FormTests
+//
+//  Created by admin on 12/03/15.
+//  Copyright (c) 2015 Ash n Pari. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Student_Feedback_FormTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
